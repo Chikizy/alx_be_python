48ed1b12d18950ddc81bc031dfe8cc244739d590
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-05-25 23:43:12 +0100

    task 1

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+#simple addition and subtraction
+
+number1 = 10
+number2 = 5
+
+sum_ans = number1 + number2
+difference_ans = number1 - number2
+print("sum of",number1 , "and",number2 , "is", sum_ans)
+print("difference of", number1 , "and",number2 , "is", sum_ans)
