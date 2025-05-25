ca7b8603acdcf773f7ab6ad81cc2729e5d377c38
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-05-25 23:52:40 +0100

    task 1 3rd redo

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,5 +5,7 @@ number2 = 5
 
 sum_ans = number1 + number2
 difference_ans = number1 - number2
+multiple = number1 * number2
 print("sum of",number1 , "and",number2 , "is", sum_ans)
 print("difference of", number1 , "and",number2 , "is", sum_ans)
+print("multiplication of", number1 , "and",number2 , "is", multiple)
