41d09640d46ef382820f906873dc72c4e81e435a
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-06-06 15:02:27 +0100

    task 1..

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,9 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 
-operand = input("Choose the operation (+, -, *, /):")
+operation = input("Choose the operation (+, -, *, /):")
 
-match operand:
+match operation:
     case "+":
         result = num1 + num2
     case "-":
@@ -17,5 +17,5 @@ match operand:
             result = num1 / num2
     case _:
         print("incorrect operator")
-if operand != "/" and num2 != 0:
+if operation != "/" and num2 != 0:
     print(f"The result is {result}")
