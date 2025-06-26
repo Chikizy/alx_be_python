8d34acf3554c83943aebad5bbf9bebdb98cc8b28
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-06-26 22:47:07 +0100

    Thank God, whew!

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,26 +4,29 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
-    
+    def return_book(self):
+        self.checked_out = False
 
 class Library:
-    def __init__(self):
-        self._books = []
+        def __init__(self):
+            self._books = []
 
-    def add_book(self, book):
-        self._books.append(book)
-       
-    def check_out_book(self, title):
-        for book in self._books:
-            if title == book.title:
-                book._is_checked_out = True
-    
-    def list_available_books(self):
-        for book in self._books:
-            if not book._is_checked_out:
-                print(f"{book.title} by {book.author}")
+        def add_book(self, book):
+            self._books.append(book)
+        
+        def check_out_book(self, title):
+            for book in self._books:
+                if title == book.title:
+                    book._is_checked_out = True
+        
+        def list_available_books(self):
+            for book in self._books:
+                if not book._is_checked_out:
+                    print(f"{book.title} by {book.author}")
 
-    def return_book(self, title):
-        for book in self._books:
-            if title == book.title:
-                book._is_checked_out = False
+        def return_book(self, title):
+            for book in self._books:
+                if title == book.title:
+                    # book._is_checked_out = False (replacing line with code below)
+                    book.return_book()
+                    return
