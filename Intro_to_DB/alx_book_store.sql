0aa66751b90258c1a3234effe140aad1dca36d93
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-08-01 00:07:48 +0100

    done

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,4 +1,8 @@
-CREATE TABLE Books (
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title varchar(130) NOT NULL,
     author_id INT FOREIGN KEY,
@@ -6,27 +10,27 @@ CREATE TABLE Books (
     publication_date DATE NOT NULL
 );
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL,
 );
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR (215) NOT NULL,
     email VARCHAR(215),
     adddress TEXT NOT NULL,
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT FOREIGN KEY,
     order_date DATE
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT FOREIGN KEY,
-    book_id INT FOREIGN KEY
+    book_id INT FOREIGN KEY,
     quantity DOUBLE
-)
+);
